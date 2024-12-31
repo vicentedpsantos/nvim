7ae9244093336e8797c6582da7f53766e336b59d
Vicente Santos <vicentedpsantos@gmail.com>
2024-12-31 12:07:16 +0000

    f

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -41,14 +41,20 @@ return {
       {"<leader>ff", "<cmd>lua require('fzf-lua').live_grep_native()<CR>", desc = "Opens live grep."},
       {"<leader>fw", "<cmd>lua require('fzf-lua').grep_cword()<CR>", desc = "Grep word under cursor."},
 
+      -- colorschemes
+      {"<leader>fac", "<cmd>lua require('fzf-lua').awesome_colorschemes()<CR>", desc = "Grep word under cursor."},
+
       -- Files
       {"<leader>t", "<cmd>lua require('fzf-lua').files()<CR>", desc = "Opens file explorer."},
 
       -- Git
-      {"<leader>gb", "<cmd>lua require('fzf-lua').git_blame()<CR>", desc = "Opens git blame."}
+      {"<leader>gb", "<cmd>lua require('fzf-lua').git_blame()<CR>", desc = "Opens git blame."},
+      {"<leader>gs", "<cmd>lua require('fzf-lua').git_status()<CR>", desc = "Opens git status."}
     }, {
-        mode = "n",
+        mode = {"n", "v"},
         silent = true,
     })
+
+    vim.keymap.set("v", "<leader>fv", "<cmd>lua require('fzf-lua').grep_visual()<CR>", { desc = "Grep visual selection."} )
   end
 }
